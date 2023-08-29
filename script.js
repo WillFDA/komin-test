1fbf38f2c87e9c3fc51c2d24f19dd147787d6d42
William <william.deazevedopro@gmail.com>
2023-08-29 11:16:28 +0200

    Figure en JS

--- script.js
+++ script.js
@@ -31,3 +31,155 @@ editButton.addEventListener("click", function () {
     }
   });
 });
+
+const data = [
+  {
+    status: "Onboarding",
+    time: "18 min",
+    description:
+      "Description : Roadbook type des procédures de recrutement des agences du Groupe Onsite",
+    thumbnail: "./assets/figure001.png",
+    figCaptionTitle:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et.",
+    figCaptionDate: "il y a 2m",
+  },
+  {
+    status: "Onboarding",
+    time: "13 min",
+    description:
+      "Description : Roadbook type des procédures de recrutement des agences du Groupe Onsite",
+    thumbnail: "./assets/figure002.png",
+    figCaptionTitle: "Sales & Marketing that really works !",
+    figCaptionDate: "il y a 7j",
+  },
+  {
+    status: "Onboarding",
+    time: "11 min",
+    description:
+      "Description : Roadbook type des procédures de recrutement des agences du Groupe Onsite",
+    thumbnail: "./assets/figure002.png",
+    figCaptionTitle:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et.",
+    figCaptionDate: "il y a 7j",
+  },
+  {
+    status: "Onboarding",
+    time: "25 min",
+    description:
+      "Description : Roadbook type des procédures de recrutement des agences du Groupe Onsite",
+    thumbnail: "./assets/figure003.png",
+    figCaptionTitle:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et.",
+    figCaptionDate: "il y a 2j",
+  },
+  {
+    status: "Onboarding",
+    time: "25 min",
+    description:
+      "Description : Roadbook type des procédures de recrutement des agences du Groupe Onsite",
+    thumbnail: "./assets/figure004.png",
+    figCaptionTitle:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et.",
+    figCaptionDate: "il y a 4j",
+  },
+  {
+    status: "Onboarding",
+    time: "25 min",
+    description:
+      "Description : Roadbook type des procédures de recrutement des agences du Groupe Onsite",
+    thumbnail: "./assets/figure005.png",
+    figCaptionTitle:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et.",
+    figCaptionDate: "il y a 4j",
+  },
+  {
+    status: "Onboarding",
+    time: "35 min",
+    description:
+      "Description : Roadbook type des procédures de recrutement des agences du Groupe Onsite",
+    thumbnail: "./assets/figure006.png",
+    figCaptionTitle:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et.",
+    figCaptionDate: "il y a 1j",
+  },
+  {
+    status: "Onboarding",
+    time: "35 min",
+    description:
+      "Description : Roadbook type des procédures de recrutement des agences du Groupe Onsite",
+    thumbnail: "./assets/figure007.png",
+    figCaptionTitle:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et.",
+    figCaptionDate: "il y a 1j",
+  },
+  {
+    status: "Onboarding",
+    time: "5 min",
+    description:
+      "Description : Roadbook type des procédures de recrutement des agences du Groupe Onsite",
+    thumbnail: "./assets/figure008.png",
+    figCaptionTitle:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et.",
+    figCaptionDate: "il y a 2h",
+  },
+  {
+    status: "Onboarding",
+    time: "55 min",
+    description:
+      "Description : Roadbook type des procédures de recrutement des agences du Groupe Onsite",
+    thumbnail: "./assets/figure009.png",
+    figCaptionTitle:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et.",
+    figCaptionDate: "il y a 4h",
+  },
+  {
+    status: "Onboarding",
+    time: "45 min",
+    description:
+      "Description : Roadbook type des procédures de recrutement des agences du Groupe Onsite",
+    thumbnail: "./assets/figure010.png",
+    figCaptionTitle:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et.",
+    figCaptionDate: "il y a 1j",
+  },
+  {
+    status: "Onboarding",
+    time: "45 min",
+    description:
+      "Description : Roadbook type des procédures de recrutement des agences du Groupe Onsite",
+    thumbnail: "./assets/figure011.png",
+    figCaptionTitle:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et.",
+    figCaptionDate: "il y a 1j",
+  },
+];
+
+const section = document.querySelector("section");
+console.log(section);
+data.forEach((figureData) => {
+  const figureHTML = `<figure>
+  <div class="img-container">
+    <div class="overlay">
+      <div class="overlay-infos">
+        <h3>${figureData.status}</h3>
+        <div class="overlay-timer-box">
+          <img src="./assets/timer-icon.svg" alt="Timer icon" />
+          <span class="video-time">${figureData.time}</span>
+        </div>
+      </div>
+
+      <p class="overlay-description">
+      ${figureData.description}
+      </p>
+    </div>
+    <img src='${figureData.thumbnail}' alt="Thumbnail" />
+  </div>
+  <figcaption>
+    <h2>
+    ${figureData.figCaptionTitle}
+    </h2>
+    <p class="date-description">${figureData.figCaptionDate}</p>
+  </figcaption>
+</figure>`;
+  section.insertAdjacentHTML("beforeend", figureHTML);
+});
